568216cb9175ef799c4096ec72c1570958509668
tpid-firma <firman@technopartne.co.id>
2024-08-03 20:00:56 +0700

    refactore dummy data

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import Button from '../Base/button/button';
+import { footerLinks } from './footerLinks';
 
 const Footer: React.FC = () => {
-  const footerLinks = [
-    { title: 'Get the IMDb app', url: '#' },
-    { title: 'Help', url: '#' },
-    { title: 'Site Index', url: '#' },
-    { title: 'IMDbPro', url: '#' },
-    { title: 'Box Office Mojo', url: '#' },
-    { title: 'IMDb Developer', url: '#' },
-    { title: 'Press Room', url: '#' },
-    { title: 'Advertising', url: '#' },
-    { title: 'Jobs', url: '#' },
-    { title: 'Conditions of Use', url: '#' },
-    { title: 'Privacy Policy', url: '#' },
-    { title: 'Your Ads Privacy Choices', url: '#' },
-  ];
-
   return (
     <footer className="bg-black text-gray-400 py-8">
       <div className="container mx-auto px-4">
@@ -24,7 +10,7 @@ const Footer: React.FC = () => {
           Unlock the full IMDb experience – Sign up now
         </Button>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
-          {footerLinks.map((link, index) => (
+          {footerLinks.map((link: any, index: number) => (
             <a key={index} href={link.url} className="hover:text-white">
               {link.title}
             </a>
---
+++ src/components/Footer/footerLinks.tsx
@@ -0,0 +1,14 @@
+export  const footerLinks = [
+  { title: 'Get the IMDb app', url: '#' },
+  { title: 'Help', url: '#' },
+  { title: 'Site Index', url: '#' },
+  { title: 'IMDbPro', url: '#' },
+  { title: 'Box Office Mojo', url: '#' },
+  { title: 'IMDb Developer', url: '#' },
+  { title: 'Press Room', url: '#' },
+  { title: 'Advertising', url: '#' },
+  { title: 'Jobs', url: '#' },
+  { title: 'Conditions of Use', url: '#' },
+  { title: 'Privacy Policy', url: '#' },
+  { title: 'Your Ads Privacy Choices', url: '#' },
+];
