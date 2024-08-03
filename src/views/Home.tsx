b189e34f2b34d4efc22f40de927f66feb361d438
tpid-firma <firman@technopartne.co.id>
2024-08-03 15:12:24 +0700

    feat: add api key and token tmdb

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,8 +4,8 @@ import VideoCard from '../components/VideoCard/VideoCard';
 import MovieCard from '../components/MovieCard/MovieCard';
 
 const topPicks = [
-  { title: "Star Wars: Episode V", posterUrl: "https://example.com/star-wars.jpg", rating: 8.7 },
-  { title: "Spider-Man: Across the Spider-Verse", posterUrl: "https://example.com/spider-man.jpg", rating: 8.6 },
+  { title: "Star Wars: Episode V", posterUrl: "https://images.unsplash.com/photo-1611787640592-ebf78080d96e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", rating: 8.7 },
+  { title: "Spider-Man: Across the Spider-Verse", posterUrl: "https://plus.unsplash.com/premium_photo-1682125157065-cbc4eb0fe0bb?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", rating: 8.6 },
   { title: "Interstellar", posterUrl: "https://example.com/interstellar.jpg", rating: 8.7 },
   { title: "Arrival", posterUrl: "https://example.com/arrival.jpg", rating: 7.9 },
   { title: "Inception", posterUrl: "https://example.com/inception.jpg", rating: 8.8 },
@@ -21,7 +21,7 @@ const HomePage: React.FC = () => (
         <FeaturedVideo
           title="'Inside Out 2' Make us Feel Every Emotion"
           subtitle="Watch the new 'Inside out' Trailer"
-          image="https://source.unsplash.com/random/1200x800?movie"
+          image="https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
       </div>
       <div className="w-1/4">
